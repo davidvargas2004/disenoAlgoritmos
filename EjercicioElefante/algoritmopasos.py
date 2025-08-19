c222b121c6dc738cab268c064be58d6760aa16b5
DAVID VARGAS <88800948+davidvargas2004@users.noreply.github.com>
2025-08-19 20:40:22 +0000

    repaso codigo

--- EjercicioElefante/algoritmopasos.py
+++ EjercicioElefante/algoritmopasos.py
@@ -3,35 +3,46 @@
 
 
 
-import math
+# import math
 
-lugarinicial = 0
+# lugarinicial = 0
 
 
-try:
-    lugarfinal = int(input("Diguite el lugar donde quieres llegar en candidad de metros:"))
+# try:
+#     lugarfinal = int(input("Diguite el lugar donde quieres llegar en candidad de metros:"))
 
     
+# except ValueError:
+#     print("¡Debe ingresar un número entero!")
+#     exit()
+
+
+# distancia =abs(lugarfinal - lugarinicial)
+# CantidadPasos = math.ceil(distancia /5)
+# print(f"El elefante da una cantidad de pasos evaluado en {CantidadPasos} pasos para llegar al final del objetivo {distancia} metros.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+import math 
+
+lugarstart= 0
+try:
+    lugarfinal=int(input("digite el numero de comienzo:"))
 except ValueError:
-    print("¡Debe ingresar un número entero!")
-    exit()
+    print("debe poner numeros enteros:")
+    exit();
 
 
-distancia =abs(lugarfinal - lugarinicial)
-CantidadPasos = math.ceil(distancia /5)
-print(f"El elefante da una cantidad de pasos evaluado en {CantidadPasos} pasos para llegar al final del objetivo {distancia} metros.")
-
-
-
-
-
-
-#COMO FUNCIONA LA FUNCION CEIL DEF
-def mi_ceil(numero):
-    parte_entera = int(numero)  # Trunca el número (elimina decimales)
-    if numero > 0 and numero != parte_entera:
-        return parte_entera + 1
-    elif numero < 0 and numero != parte_entera:
-        return parte_entera  # Para negativos, el "techo" es el entero truncado
-    else:
-        return parte_entera  # Si ya es entero, se devuelve igual
+recorrido= (lugarfinal - lugarstart)
+pasos=math.ceil(recorrido / 5)
+print(f"el elefante da una cantidad de {pasos} en una distancia de {recorrido}")
